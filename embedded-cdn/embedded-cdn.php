5855ad5fc630584aff77f2e04b7638de21f1cc53
jjmarhelps <justinm@canadahelps.org>
2025-03-20 10:22:04 -0400

    fix: further use esc_attr on sanitized args to prevent XSS

--- embedded-cdn/embedded-cdn.php
+++ embedded-cdn/embedded-cdn.php
@@ -38,8 +38,8 @@ function embeded_cdn_plugin_add_shortcode_cb($atts) {
 	$html .= ' id="ch_cdn_embed"';
 	$html .= ' type="text/javascript"';
 	$html .= ' data-cfasync="false"';
-	$html .= ' data-page-id="' . $atts["id"] . '"';
-	$html .= ' data-language="' . $atts["language"] . '"';
+	$html .= ' data-page-id="' . esc_attr($atts["id"]) . '"';
+	$html .= ' data-language="' . esc_attr($atts["language"]) . '"';
 	$html .= ' src="https://www.canadahelps.org/services/wa/js/apps/donatenow/embed.min.js"';
 	$html .= '></script>';
 	$html .= "\n";
